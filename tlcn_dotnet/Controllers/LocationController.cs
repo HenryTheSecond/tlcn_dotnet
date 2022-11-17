d777b68ab5c570154772e56eecb3ca94d26fee3c
HenryTheSecond <congtuyen2032001@gmail.com>
2022-11-17 22:58:51 +0700

    17-11-2022_Location-end-points
    - Vietnam and Global Location end points

---
+++ tlcn_dotnet/Controllers/LocationController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using tlcn_dotnet.Services;
+
+namespace tlcn_dotnet.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        private readonly ILocationService _locationService;
+        public LocationController(ILocationService locationService)
+        {
+            _locationService = locationService;
+        }
+
+        [HttpGet("VietnamLocation")]
+        public async Task<DataResponse> GetVietnamLocation()
+        { 
+            return await _locationService.GetVietnamLocation();
+        }
+        [HttpGet("GlobalLocation")]
+        public async Task<DataResponse> GetGlobalLocation()
+        {
+            return await _locationService.GetAllCountryAndCity();
+        }
+    }
+}
